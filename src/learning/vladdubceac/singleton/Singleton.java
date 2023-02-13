6549293e2ea918023c923cc64bb6e1f6bcebed15
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-14 00:58:29 +0200

    First commit

---
+++ src/learning/vladdubceac/singleton/Singleton.java
@@ -0,0 +1,27 @@
+package learning.vladdubceac.singleton;
+
+public class Singleton {
+
+    // an instance attribute
+    private int data = 0;
+
+    public Singleton() {
+    }
+
+    private static class SingletonHelper{
+        // Nested class is referenced after getInstance() is called
+        private static final Singleton UNIQUE_INSTANCE = new Singleton();
+    }
+
+    public static  Singleton getInstance(){
+        return SingletonHelper.UNIQUE_INSTANCE;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData(){
+        return data;
+    }
+}
